c500174c0b6f472eb29b2cb3f8fc3835dc037e05
uretim.abdullahyuksel <uretim.abdullahyuksel@gmail.com>
2026-01-07 15:20:19 +0300

    değişiklik yapıldı

--- 1_hand_tracking/hand_tracking.py
+++ 1_hand_tracking/hand_tracking.py
@@ -21,7 +21,6 @@ while True:
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     
     results = hands.process(imgRGB)
-    
     print(results.multi_hand_landmarks)
     
     if results.multi_hand_landmarks:
@@ -34,7 +33,7 @@ while True:
                 
                 cx, cy = int(lm.x*w), int(lm.y*h)
                 
-                #bas parmak. parmak numaraları handlms yapısından geliyor.
+                #bas parmak
                 if id==4:
                     cv2.circle(img, ((cx,cy)), 9, (255,0,0), cv2.FILLED)
                 if id==8:
@@ -52,7 +51,7 @@ while True:
     fps = 1 / (cTime - pTime)
     pTime = cTime
     cv2.putText(img, "fps:"+str(int(fps)), (10,75), cv2.FONT_HERSHEY_PLAIN, 3, ((255,0,0)),5)
-    
+    #ekrandaki yazılar eklendi
     
     cv2.imshow("img", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
